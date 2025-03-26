29a9f5c7f158b2b8b1db6881bb288f7e715b7ca1
Javier Portuga <javier.s.portugal@gmail.com>
2025-03-26 00:26:22 -0600

    update de archivo js
    se completo el ejercio de variables y el de inspeccionar con consola

---
+++ Variables_javascript/main.js
@@ -0,0 +1,39 @@
+//variable: espacio de memoria reservado para almacenar informacion importante del prohrama
+
+
+// Declaracion
+let cliente;
+
+//asignacion de valores
+cliente = "Carlos";
+
+cliente = "Samuel";
+
+console.log("cliente ",cliente);
+
+
+//Inicializador variables
+
+let edad =37;
+
+console.log("edad ",edad);
+
+
+
+//constantes: es una variable cuyo valor no puede ser modificado una vez que ha sido asignado
+
+const nacimiento = 1987;
+
+console.log("nacimiento ", nacimiento);
+
+//nacimiento = 1997;
+
+// este es un ejemplo de variables numericas, de cadena y boolenas
+
+let numero = 1991;
+let cadena = "esto es una cadena o string";
+let booleano = true;
+
+console.log("el numero es : ", numero);
+console.log("la cadena es : ", cadena);
+console.log("el booleano es : ", booleano);
